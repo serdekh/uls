820d9e65dd545b7909fbdaec7cb97cf96bf989db
Sergij Dekht <mistersergey055@gmail.com>
2023-03-11 19:01:12 +0100

    pushing a template of the project

---
+++ inc/uls.h
@@ -0,0 +1,29 @@
+/* My knowledge of how these functions work */
+
+// write        (KNOW!)               - function that prints data into a descriptor
+// malloc       (KNOW!)               - function to allocate dynamic memory
+// free         (KNOW!)               - function to free dynamic memory;
+// exit         (KNOW!)               - kills the process of a program and returns 1 or 0
+
+// malloc_size  (DON'T KNOW!)         - ?
+// strerror     (DON'T KNOW!)         - ?
+// opendir      (DON'T KNOW!)         - ?
+// readdir      (DON'T KNOW!)         - ?
+// closedir     (DON'T KNOW!)         - ?
+// stat         (DON'T KNOW!)         - ?
+// lstat        (DON'T KNOW!)         - ?
+// getpwuid     (DON'T KNOW!)         - ?
+// getgrgid     (DON'T KNOW!)         - ?
+// listxattr    (DON'T KNOW!)         - ?
+// getxattr     (DON'T KNOW!)         - ?
+// readlink     (DON'T KNOW!)         - ?
+// ioctl        (DON'T KNOW!)         - ?
+// issaty       (DON'T KNOW!)         - ?
+// acl_get_file (DON'T KNOW!)         - ?
+// acl_to_text  (DON'T KNOW!)         - ?
+// acl_free     (DON'T KNOW!)         - ?
+
+// perror       (NOT COMPLETELY KNOW) - function to print error in stderr. Maybe works with errno?
+// time         (NOT COMPLETELY KNOW) - function to calculate how long program works?
+// ctime        (NOT COMPLETELY KNOW) - function to calculate how long program works? 
+// errno        (NOT COMPLETELY KNOW) - saves error to a global variable?
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "../inc/libmx/inc/libmx.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        mx_printstr("usage: uls [-l] [file ...]\n");
+        return 0;
+    }
+
+    mx_printstr("![Not implemented uls]!\n");
+
+    return 0;
+}
