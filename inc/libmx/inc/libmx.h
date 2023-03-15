8473492860dbb7b787d69c5e5499e724281ee2c1
Sergij Dekht <mistersergey055@gmail.com>
2023-03-15 11:01:30 +0100

    added a function to print a string and a char after it

--- inc/libmx/inc/libmx.h
+++ inc/libmx/inc/libmx.h
@@ -110,3 +110,5 @@ void mx_pop_back(t_list **head);
 int mx_list_size(t_list *list);
 t_list *mx_sort_list(t_list *lst, bool (*cmp)(void *, void *));
 
+void mx_printstrc(const char *s, const char c);
+
