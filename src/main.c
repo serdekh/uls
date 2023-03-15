848c0f5704c6cd89c5b263a98c60bd116bdc04fc
Sergij Dekht <mistersergey055@gmail.com>
2023-03-15 14:29:57 +0100

    separated functions from the main.c file

--- src/main.c
+++ src/main.c
@@ -1,57 +1,23 @@
 #include "../inc/uls.h"
-#include "../inc/libmx/inc/libmx.h"
 
-bool mx_is_hidden_file(const char *file_name) {
-    return (file_name[0] == '.');
-}
-
-int print_files_and_directories(bool hidden_are_printed) {
-    DIR *dir = NULL;
-    t_dirent *entry = NULL;
-
-    dir = opendir(CURRENT_DIRECTORY);
-
-    if (!dir) {
-        /*
-            FIXME: Implement the correct way of error hangling by errno
-        */
-        perror("Error: can't open current directory\n");
-
-        return 1;
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (hidden_are_printed) {
-            mx_printstrc(entry->d_name, ' ');
-        }
-
-        if (!mx_is_hidden_file(entry->d_name) && !hidden_are_printed) {
-            mx_printstrc(entry->d_name, ' ');
-        }
-    }
-
-    closedir(dir);
-
-    return 0;
-}
+/*
+ * TODO: find out how to sort files and directories, 
+ * how to implement -l flag and learn functions
+ * for it
+*/
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
-        return print_files_and_directories(false);
+        print_files_and_directories(false);
+        return 0;
     }
 
     if (argc == 2) {
         if (mx_strcmp(argv[1], FLAG__HIDDEN_FILES_PRINTED) == 0) {
             print_files_and_directories(true);
+            return 0;
         }
     }
-
-    // if (argc < 3) {
-    //     mx_printstr("usage: uls [-l] [file ...]\n");
-    //     return 0;
-    // }
-
-    if (argv) {} // if statement to avoid the clang unused error
-
+    
     return 0;
 }
