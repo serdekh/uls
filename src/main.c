3389c17a80286e193e282947fa88c5db72089d4b
Sergij Dekht <mistersergey055@gmail.com>
2023-03-15 19:10:09 +0100

    added list functions to print files/dirs and sort them

--- src/main.c
+++ src/main.c
@@ -8,13 +8,20 @@
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
-        print_files_and_directories(false);
+        t_list *entries = mx_get_dirent_structures(CURRENT_DIRECTORY);
+
+        mx_sort_dirent_structures(entries);
+        mx_print_files_and_directories(entries, false);
+        mx_free_dirent_structures(entries);
+
         return 0;
     }
 
     if (argc == 2) {
+        t_list *entries = mx_get_dirent_structures(CURRENT_DIRECTORY);
+
         if (mx_strcmp(argv[1], FLAG__HIDDEN_FILES_PRINTED) == 0) {
-            print_files_and_directories(true);
+            mx_print_files_and_directories(entries, true);
             return 0;
         }
     }
