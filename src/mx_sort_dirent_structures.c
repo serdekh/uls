1bdd50c075811368671e468baa7816dfb75ef253
Sergij Dekht <mistersergey055@gmail.com>
2023-03-15 19:15:46 +0100

    added function to sort a list of dirent structures by names

---
+++ src/mx_sort_dirent_structures.c
@@ -0,0 +1,13 @@
+#include "../inc/uls.h"
+
+bool compare_dirent_name_fields(void *file1, void *file2) {
+    return (
+        mx_strcmp(
+            ((t_dirent *)file1)->d_name, ((t_dirent *)file2)->d_name
+        ) > 0
+    );
+}
+
+void mx_sort_dirent_structures(t_list *dirent_structures) {
+    mx_sort_list(dirent_structures, compare_dirent_name_fields);
+}
