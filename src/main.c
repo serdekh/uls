e1df9ae1fd9369f5f2318044c1d5b1bf4150edfb
Sergij Dekht <mistersergey055@gmail.com>
2023-03-15 11:04:20 +0100

    implemeted a 'ls' with 'no flags' and '-a' flag. Requires to sort

--- src/main.c
+++ src/main.c
@@ -1,12 +1,57 @@
+#include "../inc/uls.h"
 #include "../inc/libmx/inc/libmx.h"
 
-int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        mx_printstr("usage: uls [-l] [file ...]\n");
-        return 0;
+bool mx_is_hidden_file(const char *file_name) {
+    return (file_name[0] == '.');
+}
+
+int print_files_and_directories(bool hidden_are_printed) {
+    DIR *dir = NULL;
+    t_dirent *entry = NULL;
+
+    dir = opendir(CURRENT_DIRECTORY);
+
+    if (!dir) {
+        /*
+            FIXME: Implement the correct way of error hangling by errno
+        */
+        perror("Error: can't open current directory\n");
+
+        return 1;
     }
 
-    mx_printstr("![Not implemented uls]!\n");
+    while ((entry = readdir(dir)) != NULL) {
+        if (hidden_are_printed) {
+            mx_printstrc(entry->d_name, ' ');
+        }
+
+        if (!mx_is_hidden_file(entry->d_name) && !hidden_are_printed) {
+            mx_printstrc(entry->d_name, ' ');
+        }
+    }
+
+    closedir(dir);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        return print_files_and_directories(false);
+    }
+
+    if (argc == 2) {
+        if (mx_strcmp(argv[1], FLAG__HIDDEN_FILES_PRINTED) == 0) {
+            print_files_and_directories(true);
+        }
+    }
+
+    // if (argc < 3) {
+    //     mx_printstr("usage: uls [-l] [file ...]\n");
+    //     return 0;
+    // }
+
+    if (argv) {} // if statement to avoid the clang unused error
 
     return 0;
 }
