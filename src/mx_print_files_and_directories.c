e44076f70b0642d6729c33180259c5e92fe0c1cb
Sergij Dekht <mistersergey055@gmail.com>
2023-03-16 18:37:44 +0100

    added: if statement for a NULL pointer

--- src/mx_print_files_and_directories.c
+++ src/mx_print_files_and_directories.c
@@ -6,7 +6,7 @@ void mx_print_files_and_directories(t_list *dirent_structures, bool hidden_are_p
     for (t_list *i = dirent_structures; i != NULL; i = i->next) {
         t_dirent *temp = (t_dirent *)(i->data);
 
-        if (mx_is_hidden_file(temp->d_name) && hidden_are_printed) {
+        if (hidden_are_printed) {
             mx_printstrc(temp->d_name, ' ');
             mx_printchar(' ');
         }
