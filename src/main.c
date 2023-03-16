a1b2713406ef91791d6af20fdb30ee1bd89b9626
Sergij Dekht <mistersergey055@gmail.com>
2023-03-16 20:39:12 +0100

    modified: sorted a list of files/dirs for  flag

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[]) {
         t_list *entries = mx_get_dirent_structures(CURRENT_DIRECTORY);
 
         if (mx_strcmp(argv[1], FLAG__HIDDEN_FILES_PRINTED) == 0) {
+            mx_sort_dirent_structures(entries);
             mx_print_files_and_directories(entries, true);
             return 0;
         }
