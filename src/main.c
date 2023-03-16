7c9c0c63c681c45bdd196925b2842aaf7cf1b1f0
Sergij Dekht <mistersergey055@gmail.com>
2023-03-16 22:07:00 +0100

    modified: added mx_get_flags() and mx_free_flags() functions as a test

--- src/main.c
+++ src/main.c
@@ -10,6 +10,11 @@
 
 int main(int argc, char *argv[]) {
     if (argc == 1) mx_uls_no_params();
+
+    // temporary block to test mx_get_flags function
+    t_list *flags = mx_get_flags(argc, argv);
+
+    mx_free_flags(flags);
     
     if (argc == 2) {
         t_list *entries = mx_get_dirent_structures(CURRENT_DIRECTORY);
@@ -17,6 +22,7 @@ int main(int argc, char *argv[]) {
         if (mx_strcmp(argv[1], FLAG__HIDDEN_FILES_PRINTED) == 0) {
             mx_sort_dirent_structures(entries);
             mx_print_files_and_directories(entries, true);
+            mx_free_dirent_structures(entries);
             return 0;
         }
         else {
