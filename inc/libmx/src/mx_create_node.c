1f2580688991b0cccdec2605589b00f4e43dceed
Sergij Dekht <mistersergey055@gmail.com>
2023-03-16 22:04:08 +0100

    modified: making a bit more readable

--- inc/libmx/src/mx_create_node.c
+++ inc/libmx/src/mx_create_node.c
@@ -1,9 +1,12 @@
 #include "../inc/libmx.h"
 
 t_list *mx_create_node(void *data) {
-	t_list *node = malloc(sizeof(t_list));
-	node->data = data;
-	node->next = NULL;
-	return node;
+	t_list *node = NULL;
+    node = malloc(sizeof(t_list));
+
+    node->data = data;
+    node->next = NULL;
+
+    return node;
 }
 
