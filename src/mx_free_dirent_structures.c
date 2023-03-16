1af326d179e34825136a1a1419a0a58b0e594aca
Sergij Dekht <mistersergey055@gmail.com>
2023-03-16 22:08:50 +0100

    modified: solved memory leak problem by freeing nodes

--- src/mx_free_dirent_structures.c
+++ src/mx_free_dirent_structures.c
@@ -2,10 +2,12 @@
 
 void mx_free_dirent_structures(t_list *dirent_structures_list) {
     if (!dirent_structures_list) return;
-    
+
     for (t_list *i = dirent_structures_list; i != NULL; i = i->next) {
-        free((t_dirent*)(i->data));
+        free(i->data);
     }
 
-    free(dirent_structures_list);
+    while (dirent_structures_list != NULL) {
+        mx_pop_front(&dirent_structures_list);
+    }
 }
