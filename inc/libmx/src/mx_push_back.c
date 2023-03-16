9bf9d7775855e637672e80bfc3b6a670fdfa08f6
Sergij Dekht <mistersergey055@gmail.com>
2023-03-16 22:04:45 +0100

    modified: making a bit more readable

--- inc/libmx/src/mx_push_back.c
+++ inc/libmx/src/mx_push_back.c
@@ -1,13 +1,19 @@
 #include "../inc/libmx.h"
 
 void mx_push_back(t_list **list, void *data) {
-	t_list *new_node = mx_create_node(data);
-	t_list *last_node = *list;
-	if (last_node != NULL) {
-		for ( ; last_node->next != NULL; last_node = last_node->next) { }
-		last_node->next = new_node;
-	} else {
-		*list = new_node;
-	}
+	t_list *node = mx_create_node(data);
+
+    if (*list == NULL) {
+        *list = node;
+        return;
+    }
+
+    t_list *temporary = *list;
+
+    while (temporary->next != NULL) {
+        temporary = temporary->next;
+    }
+
+    temporary->next = node;
 }
 
