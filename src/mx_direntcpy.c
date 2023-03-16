093fc2c9f719dfac0e60651764d3f4cc92def1c3
Sergij Dekht <mistersergey055@gmail.com>
2023-03-16 22:07:52 +0100

    modified: making a bit more readable

--- src/mx_direntcpy.c
+++ src/mx_direntcpy.c
@@ -2,12 +2,11 @@
 
 void mx_direntcpy(t_dirent *dest, t_dirent *src) {
     if (!dest || !src) return;
-    
-    dest->d_ino = src->d_ino;
 
     mx_strcpy(dest->d_name, src->d_name);
 
+    dest->d_ino = src->d_ino;
     dest->d_off = src->d_off;
-    dest->d_reclen = src->d_reclen;
     dest->d_type = src->d_type;
+    dest->d_reclen = src->d_reclen;
 }
