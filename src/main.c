fb12a8605962fc125f55f8076bc1e60bbb8e0b9a
Sergij Dekht <mistersergey055@gmail.com>
2023-03-17 11:36:41 +0100

    modified: removed some tasks and changed behaviour for uls with params

--- src/main.c
+++ src/main.c
@@ -2,36 +2,23 @@
 
 /*
  * (1) TODO: Implement separating flags and dir/files from each other
+ *           By printing firstly the files and the contains of dirs
  * (2) TODO: Implement -l flag
- * (3) TODO: Implement poliflags input (-a -l)
- * (4) TODO: Improve Makefile
- * (5) TODO: Impelement '| cat -e' setting
+ * (3) TODO: Improve Makefile
+ * (4) TODO: Impelement '| cat -e' setting
+*/
+
+/*
+    PLANNING TO DO: implement separation files and directories
+    to print file names and then print files from directories
 */
 
 int main(int argc, char *argv[]) {
+    if (argv) {} // to avoid clang error
+    
     if (argc == 1) mx_uls_no_params();
 
-    // temporary block to test mx_get_flags function
-    t_list *flags = mx_get_flags(argc, argv);
+    if (argc >= 2) mx_uls_with_params();
 
-    mx_free_flags(flags);
-    
-    if (argc == 2) {
-        t_list *entries = mx_get_dirent_structures(CURRENT_DIRECTORY);
-
-        if (mx_strcmp(argv[1], FLAG__HIDDEN_FILES_PRINTED) == 0) {
-            mx_sort_dirent_structures(entries);
-            mx_print_files_and_directories(entries, true);
-            mx_free_dirent_structures(entries);
-            return 0;
-        }
-        else {
-            mx_printstr("uls: illegal option -- ");
-            mx_printstr(&argv[1][1]);
-            mx_printstr("\nusage: uls [-l] [file ...]\n");
-            mx_free_dirent_structures(entries);
-        }
-    }
-    
     return 0;
 }
