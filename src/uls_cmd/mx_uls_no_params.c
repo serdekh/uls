01124448e7b7507cb25870ff6218dbf9980dada3
Sergij Dekht <mistersergey055@gmail.com>
2023-03-20 22:47:47 +0100

    modified: added if-statement for a NULL pointer

--- src/uls_cmd/mx_uls_no_params.c
+++ src/uls_cmd/mx_uls_no_params.c
@@ -1,7 +1,11 @@
 #include "../../inc/uls.h"
 
-void mx_uls_no_params() {
-    t_list *entries = mx_get_dirent_structures(CURRENT_DIRECTORY);
+void mx_uls_no_params(char *path) {
+    if (!path) {
+        path = CURRENT_DIRECTORY;
+    }
+    
+    t_list *entries = mx_get_dirent_structures(path);
 
     mx_sort_dirent_structures(entries);
     mx_print_files_and_directories(entries, false);
