d2f2888e94d5660907d42ee14c30067124371ab0
Sergij Dekht <mistersergey055@gmail.com>
2023-03-20 22:49:51 +0100

    modified: added a function to check for -l flag and used mx_handle_l_flag in the main function

--- src/uls_cmd/mx_uls_with_params.c
+++ src/uls_cmd/mx_uls_with_params.c
@@ -1,5 +1,26 @@
 #include "../../inc/uls.h"
 
-void mx_uls_with_params() {
-    exit(0);
+void mx_check_l_flag(char *argv[]) {
+    if (!argv) return;
+
+    bool flag_l_found = mx_strcmp(argv[1], "-l") == 0;
+
+    if (flag_l_found) {
+        return;
+    }
+    if (!flag_l_found) {
+        write(2, ULS_USAGE, strlen(ULS_USAGE));
+        exit(0);
+    }
+}
+
+void mx_uls_with_params(int argc, char *argv[]) {
+    if (argc) {}
+    //FIXME: need to check what input we get
+
+    mx_check_l_flag(argv);
+
+    t_list *dirent_structures = mx_get_dirent_structures(CURRENT_DIRECTORY);
+
+    mx_handle_l_flag(dirent_structures);
 }
