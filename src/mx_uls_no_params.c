4d03b5f6f3db1b7cf7eb19be51c9c0bad0801091
Sergij Dekht <mistersergey055@gmail.com>
2023-03-22 14:36:02 +0100

    mofidied: changed a definition of mx_print_files_and_directories()

--- src/mx_uls_no_params.c
+++ src/mx_uls_no_params.c
@@ -9,7 +9,7 @@ void mx_uls_no_params(char *path) {
     t_list *entries = mx_get_dirent_structures(path);
 
     mx_sort_dirent_structures(entries);
-    mx_print_files_and_directories(entries, false);
+    mx_print_files_and_directories(entries);
     mx_free_dirent_structures(entries);
 
     exit(SUCCESS);
