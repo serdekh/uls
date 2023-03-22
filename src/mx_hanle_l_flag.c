2e63bf1c4acea18d6faa8eb3923c7713550ed292
Sergij Dekht <mistersergey055@gmail.com>
2023-03-22 13:46:17 +0100

    modified: added mx_check_l_flag()

--- src/mx_hanle_l_flag.c
+++ src/mx_hanle_l_flag.c
@@ -1,4 +1,27 @@
 #include "../inc/uls.h"
+#include "../inc/uls_error.h"
+
+void mx_check_l_flag(int argc, char **argv) {
+    if (!argv || argc <= 0) return;
+
+    mx_check_flag_l_position(argc, argv);
+
+    bool flag_l_found = mx_strcmp(argv[1], "-l") == 0;
+
+    if (flag_l_found) {
+        return;
+    }
+
+    bool invalid_flag = (mx_is_flag(argv[1]) && argv[1][0] != 'l');
+
+    if (invalid_flag) {
+        mx_print_invalid_option(argv);
+    }
+
+    mx_print_dirents(argc, argv);
+
+    exit(SUCCESS);
+}
 
 void mx_handle_l_flag(t_list *dirent_structures) {
     if (!dirent_structures) return;
