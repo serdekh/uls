b06e156ef3f48593fc0a7143f4754d89a32cc6b4
Sergij Dekht <mistersergey055@gmail.com>
2023-03-22 14:36:42 +0100

    added: a header with foreach functions to iterate dirents

---
+++ inc/uls_foreach.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "./uls.h"
+
+void mx_foreach_t_dirent(t_list *dirent_structures, void (*f)(t_dirent *));
+void mx_foreach_t_dirent_and_iterator(t_list *dirent_structures, void (*f)(t_dirent *, t_list *i));
+void mx_foreach_t_dirent_in_list(t_list *dirent_structures, void (*f)(t_dirent *, t_list *dirent_structures));
+
+void mx_foreach_free(t_dirent *temp);
+void mx_foreach_print_name(t_dirent *temp);
+void mx_foreach_print_folder(t_dirent *temp);
+void mx_foreach_print_name_newline(t_dirent *temp); 
+
+void mx_foreach_print_dirent(t_dirent *temp, t_list *i);
+
