e10c049d32d6d2611f0ef09498061fb5d44d6af0
Sergij Dekht <mistersergey055@gmail.com>
2023-04-27 12:29:56 +0200

    Modify error message

--- src/mx_error_handling.c
+++ src/mx_error_handling.c
@@ -5,7 +5,7 @@ void mx_check_flag_l_position(int argc, char **argv) {
     for (int i = 2; i < argc; i++) {
         if (mx_strcmp(argv[i], "-l") == 0) {
             mx_printerr(ULS_USAGE);
-            exit(FAILURE);
+            exit(EXIT_FAILURE);
         }
     }
 }
@@ -15,5 +15,5 @@ void mx_print_invalid_option(char **argv) {
     mx_printerr(&argv[1][1]); // &argv[1][1] -- a flag without '-' symbol
     mx_printerr("\n");
     mx_printerr(ULS_USAGE);
-    exit(FAILURE);
+    exit(EXIT_FAILURE);
 }
