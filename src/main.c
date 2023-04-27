7c2423292f2decd084dd5a50dafd170aeaef1278
Sergij Dekht <mistersergey055@gmail.com>
2023-04-27 12:29:38 +0200

    Remove one task

--- src/main.c
+++ src/main.c
@@ -1,24 +1,20 @@
 #include "../inc/uls.h"
 
 /*
- * (1) TODO: Implement separating flags and dir/files from each other
- *           By printing firstly the files and the contains of dirs
  * (3) TODO: Improve Makefile
  * (4) TODO: Impelement '| cat -e' setting
  * 
+ * (1) DONE (MAYBE): Implement separating flags and dir/files from each other
+ *           By printing firstly the files and the contains of dirs
  * (2) DONE: Implement -l flag
 */
 
-/*
-    PLANNING TO DO: implement separation files and directories
-    to print file names and then print files from directories
-*/
-
 int main(int argc, char *argv[]) {
-    // t_dirent *dirent = mx_get_dirent_structure("inc/uls.h");
+    // struct winsize w;
+    // ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
-    // mx_printstrc(dirent->d_name, '\n');
-    // free(dirent);
+    // printf ("lines %d\n", w.ws_row);
+    // printf ("columns %d\n", w.ws_col);
     
     if (argc == 1) mx_uls_no_params(CURRENT_DIRECTORY);
     if (argc >= 2) mx_uls_with_params(argc, argv);
