53865bd6552957d14542ac551b9fde6cda513aaf
Sergij Dekht <mistersergey055@gmail.com>
2023-04-29 17:33:13 +0200

    Change printing for cat -e option

--- inc/uls_foreach.h
+++ inc/uls_foreach.h
@@ -7,7 +7,7 @@ void mx_foreach_t_dirent_in_list(t_list *dirent_structures, void (*f)(t_dirent *
 void mx_foreach_print_dirent(t_dirent *temp, t_list *i);
 
 void mx_t_dirent_free(t_dirent *temp);
-void mx_t_dirent_print_name(t_dirent *temp);
+void mx_t_dirent_print_name(t_dirent *temp, t_list *i);
 void mx_t_dirent_print_folder(t_dirent *temp);
 void mx_t_dirent_print_name_newline(t_dirent *temp); 
 
