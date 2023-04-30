bfe1dc90c8bcce51e32966c281f25b75f69b69c3
Sergij Dekht <mistersergey055@gmail.com>
2023-04-30 14:55:50 +0200

    Add a function to get winsize

--- inc/utils.h
+++ inc/utils.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "../inc/libmx/inc/libmx.h"
 
+t_winsize mx_get_winsize(); 
+
 char *mx_add_strings(char *str1, char *str2);
 char *mx_remove_last_directory_move(char *path);
 char *mx_get_last_file_or_directory(char *path);
