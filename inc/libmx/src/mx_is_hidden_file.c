e32676e560e87516742bf77ed887257154067240
Sergij Dekht <mistersergey055@gmail.com>
2023-04-30 14:54:48 +0200

    Solve a bug when ../ is considered to be a hidden file

--- inc/libmx/src/mx_is_hidden_file.c
+++ inc/libmx/src/mx_is_hidden_file.c
@@ -1,5 +1,13 @@
 #include "../inc/libmx.h"
 
 bool mx_is_hidden_file(const char *file_name) {
-    return (file_name[0] == '.');
+    if (!file_name) return false;
+
+    int len = strlen(file_name);
+
+    if (len == 1 && file_name[0] == '.') return true;
+    if (len == 2 && file_name[0] == '.' && file_name[1] == '.') return true;
+    if (len > 2 && file_name[0] == '.') return true;
+    
+    return false;
 }
